6111b519e948bf85fda6ad99d44e5de5802a1c6f
chris-macharia <christophermaina003@gmail.com>
2022-10-12 04:32:11 +0300

    Kick start the project

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>MAGIC STORES</title>
+  <link rel="stylesheet" href="/product-crud/index.css">
+</head>
+
+<body>
+  <div id="navbar">
+    <h1>HELLO THERE</h1>
+  </div>
+
+  <form action="">
+    <div>
+      Product title: <br>
+      <input type="text">
+    </div>
+  </form>
+</body>
+
+</html>
