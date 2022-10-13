8503c1853978663cb9c21582ed13288586fbb7b3
chris-macharia <christophermaina003@gmail.com>
2022-10-13 12:27:03 +0300

    added connection to the DB but the name column doesn't work. Also, the connection is not secure.

--- Products/phones-POST.php
+++ Products/phones-POST.php
@@ -1,6 +1,17 @@
 
-<?php 
-  echo $_POST["product-id"];
-  echo $_POST["product-name"];
-  echo $_POST["product-quantity"];
+<?php
+
+$pdo = new PDO('mysql:host=127.0.0.1;port=3306;dbname=magic-stores', 'root', '');
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+$productId =  $_POST['product-id'];
+$productName = $_POST['product-name'];
+$productQuantity = $_POST['product-quantity'];
+
+$statement = $pdo->prepare("INSERT INTO phones (ID,QUANTITY)
+                          VALUES($productId, $productQuantity)"); /* product name not working */
+$statement->execute();                          
+
+echo "SUBMITTED"                          
 ?>
