5af0bb48cb91753296ae88204561f2a0d6fb8e36
chris-macharia <christophermaina003@gmail.com>
2022-10-18 10:14:10 +0300

    added a WRITE method to the DB. The method however allows for empty writing into the DB hence some validation is required.

--- create/create.php
+++ create/create.php
@@ -2,7 +2,26 @@
 $pdo = new PDO('mysql:host=localhost;port=3306;dbname=magic-stores', 'root', ''); // connecting to the DB
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+if ($_SERVER["REQUEST_METHOD"] === "POST") { //this if statement prevents errors of empty variables from the unfilled forms
+  $title = $_POST['title'];
+  $image = "";
+  $description = $_POST['description'];
+  $price = $_POST['price'];
+  $date = date('Y-M-d H:i:s');
+
+  $statement = $pdo->prepare("INSERT INTO laptops (title, description, image, price, create_date)
+              VALUES(:title, :description, :image, :price, :create_date)");
+
+  $statement->bindValue(':title', $title);  //this method is secure as it prevent SQL injection.
+  $statement->bindValue(':description', $description);
+  $statement->bindValue(':image', $image);
+  $statement->bindValue(':price', $price);
+  $statement->bindValue(':create_date', $date);
+  $statement->execute();
+}
+
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -22,27 +41,27 @@ $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   </div>
 
   <div>
-    <form id="formDiv" action="" method="POST">
-    <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Product Image</label>
+    <form id="formDiv" action="" method="post">
+      <div class="mb-3">
+        <label class="form-label">Product Image</label>
         <br>
-        <input type="file">
+        <input type="file" name="image">
       </div>
       <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Product Title</label>
-        <input type="text" class="form-control" id="exampleInputEmail1">
+        <label class="form-label">Product Title</label>
+        <input type="text" class="form-control" name="title">
       </div>
       <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Product Description</label>
-        <textarea class="form-control" id="exampleInputEmail1"></textarea>
+        <label class="form-label">Product Description</label>
+        <textarea class="form-control" name="description"></textarea>
       </div>
       <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Product price</label>
-        <input type="number" step=".01" class="form-control" id="exampleInputEmail1">
+        <label class="form-label">Product price</label>
+        <input type="number" step=".01" class="form-control" name="price">
       </div>
       <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Record create date</label>
-        <input type="date" class="form-control" id="exampleInputEmail1">
+        <label class="form-label">Record create date</label>
+        <input type="date" class="form-control" name="date">
       </div>
       <br>
       <button type="submit" class="btn btn-primary">Submit</button>
