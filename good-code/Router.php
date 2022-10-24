ff939fddc62a12ccf0ee9445b17bfda829d42191
chris-macharia <christophermaina003@gmail.com>
2022-10-24 09:30:10 +0300

    created a controller for the products and a router for the server to be deployed in the public folder, which is the only folder that should be accessible by the users

--- good-code/Router.php
+++ good-code/Router.php
@@ -3,5 +3,35 @@
 namespace app;
 
 class Router{
+
+  public array $getRoute = [];
+  public array $postRoute = [];
   
+  public function get($url, $fn){
+    $this->getRoute[$url] = $fn;
+  }
+
+  public function post($url, $fn){
+    $this->postRoute[$url] = $fn;
+  }
+
+  public function resolve(){
+    $currentUrl = $_SERVER['PATH_INFO'] ?? '/';  //path info doesn't change
+    $method = $_SERVER['REQUEST_METHOD'];
+
+    if ($method === 'GET'){
+      $fn = $this->getRoute[$currentUrl] ?? null;
+    }else{
+      $fn = $this->postRoute[$currentUrl] ?? null;
+    }
+
+    if($fn){
+      call_user_func($fn);
+    }else{
+      echo "Page not found";
+    }
+  }
+
+  
+
 }
--- good-code/controllers/ProductController.php
+++ good-code/controllers/ProductController.php
@@ -3,8 +3,21 @@ namespace app\controllers;
 
 class ProductController{
 
-  public function index(){
-    
+  public static function index(){
+    echo "index"; 
   }
+
+  public static  function create(){
+    echo "create page";
+  }
+
+  public static  function update(){
+    echo "update page";
+  }
+
+  public static  function delete(){
+    echo "delete page";
+  }
+  
 
 }
